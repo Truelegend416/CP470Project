305054a6612b50cad1e60035a674628da2f0a084
Manav Desai <58055256+manavdesai17@users.noreply.github.com>
2021-12-08 22:11:58 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/cp470groupproject/Adapter/HabitAdapter.java
@@ -0,0 +1,18 @@
+package com.example.cp470groupproject.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.cp470groupproject.Habit;
+import com.example.cp470groupproject.HabitDatabase;
+import com.example.cp470groupproject.HabitTrackerActivity;
+import com.example.cp470groupproject.R;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
